654363233d7e6a6e7bc4f046d10ab9c781bd9358
Zinc Glaze <zekegraves@gmail.com>
2018-11-06 17:19:59 -0500

    set up initial files and packages

---
+++ liri.js
@@ -0,0 +1,8 @@
+//read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+//import the `keys.js` file and store it in a variable
+var spotify = new Spotify(keys.spotify);
+
+
+
