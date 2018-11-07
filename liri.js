0b04bd494daa13aec083f2b74a4315894daf1be4
Zinc Glaze <zekegraves@gmail.com>
2018-11-07 16:05:32 -0500

    set up switch-case statement and functions

--- liri.js
+++ liri.js
@@ -4,5 +4,69 @@ require("dotenv").config();
 //import the `keys.js` file and store it in a variable
 var spotify = new Spotify(keys.spotify);
 
+//Requires request npm package
+var request = require("request");
+
+//Assigns user input to variables
+var nodeArgs = process.argv;
+var command = nodeArgs[2];
+
+//Switch-case statement runs a function according to user input
+switch (command) {
+case "concert-this":
+  concertThis();
+  break;
+
+case "spotify-this":
+  spotifyThis();
+  break;
+
+case "movie-this":
+  movieThis();
+  break;
+
+case "do-what-it-says":
+  doWhatItSays();
+  break;
+}
+
+//Functions
+
+function concertThis() {
+  
+}
+
+function spotifyThis() {
+
+}
+
+function movieThis() {
+  //creates empty variable for movie name
+  var movieName = "";
+  //loops through node arguments to assemble the title into a string
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      movieName = movieName + "+" + nodeArgs[i];
+    }
+    else {
+      movieName += nodeArgs[i];
+    }
+  }
+  //Creates query URL
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  //requests and prints movie info
+  request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+      console.log("The movie's director is: " + JSON.parse(body).Director);
+      console.log("The movie's actors are: " + JSON.parse(body).Actors);
+    }
+  });
+}
+
+function doWhatItSays() {
+
+}
+
 
 
